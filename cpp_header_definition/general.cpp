baa65bab47122331af1e8179788d0cd8b094e312
JM21341 <fjohnmiguel@gmail.com>
2026-05-01 09:57:34 +0800

    Feature: Adding file-based records system

--- cpp_header_definition/general.cpp
+++ cpp_header_definition/general.cpp
@@ -7,5 +7,29 @@
 #include "../header/classes.h"
 #include "../header/structs.h"
 
-Structs::Node* General::readDataFromFile(Structs::Node* head);
+Structs::Node* General::readDataFromFile(Structs::Node* head){
+    if(head != NULL){
+        std::cout << "Something went wrong. Data already pre-exists in the program before running." << std::endl;
+        return NULL;
+    }
+
+    std::string line;
+
+    std::ifstream file("../records/user.csv"); // a bit dangerous of a directory
+
+    // checks if file did not open
+    if(!file.is_open()){
+        std::cerr << "File did not open in readDataFromFile." << std::endl;
+        return NULL;
+    }
+
+    while(std::getline(file, line)){
+
+    }
+
+    file.close();
+
+    return head;
+}
+
 std::string General::login(Structs::Node* head);
