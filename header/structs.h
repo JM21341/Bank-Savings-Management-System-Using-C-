6b4251e0c9ad3a3ce2a2d46a1f0750634297764f
JM21341 <fjohnmiguel@gmail.com>
2026-05-01 19:05:15 +0800

    Feature: Added transaction feature

--- header/structs.h
+++ header/structs.h
@@ -4,8 +4,14 @@
 #include "classes.h"
 
 namespace Structs{
+    struct Transaction{
+        int transaction_num;
+        std::string transaction_id, details, date;
+    };
+
     struct Node{
         Classes::User data;
+        Transaction transaction;
         Node* next;
     };
 }
